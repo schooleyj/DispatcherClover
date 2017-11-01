681987a1085a3fec0ec6362837b89bc06af9c04f
Johan Burke <jburke4126@gmail.com>
2017-11-01 15:29:43 -0400

    Added account sign in code and filled in JSON create job request

--- app/src/main/java/com/example/schoo_000/dispatchalpha_v1/MainActivity.java
+++ app/src/main/java/com/example/schoo_000/dispatchalpha_v1/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.schoo_000.dispatchalpha_v1;
 
+import android.accounts.Account;
+import android.os.AsyncTask;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +15,7 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.widget.Toast;
 // imports for connecting to the server
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,14 +24,26 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.clover.sdk.util.CloverAccount;
+import com.clover.sdk.v1.BindingException;
+import com.clover.sdk.v1.ClientException;
+import com.clover.sdk.v1.ServiceException;
+import com.clover.sdk.v1.merchant.Merchant;
+import com.clover.sdk.v1.merchant.MerchantConnector;
 
 // for now have to do the account thing in the main activity
 public class MainActivity extends AppCompatActivity
 {
+    private Account account;
+    private MerchantConnector merchantConnector;
 
     private EditText jobNameEditText;
     private EditText descriptionEditText;
     private Button submitButton;
+
+    private String merchantID;
+    private String merchantAddress;
+    private String merchantPhone;
 
     private String jobNameString = "";
     private String descriptionString = "";
@@ -49,6 +65,7 @@ public class MainActivity extends AppCompatActivity
 
     public void onPause()
     {
+        disconnect();
         Editor editor = savedValues.edit();
         editor.putString("jobNameString", jobNameString);
         editor.putString("descriptionString", descriptionString);
@@ -60,24 +77,83 @@ public class MainActivity extends AppCompatActivity
     {
         super.onResume();
 
+        if (account == null) {
+            account = CloverAccount.getAccount(this);
+
+            if (account == null) {
+                Toast.makeText(this, getString(R.string.no_account),
+                        Toast.LENGTH_SHORT).show();
+                finish();
+                return;
+            }
+        }
+
+        connect();
+
+        getMerchant();
+
         jobNameString = savedValues.getString("jobNameString", "");
         jobNameEditText.setText(jobNameString);
         descriptionString = savedValues.getString("descriptionString", "");
         descriptionEditText.setText(descriptionString);
     }
-        //Test Comment
 
+    private void connect() {
+        disconnect();
+        if (account != null) {
+            merchantConnector = new MerchantConnector(this, account, null);
+            merchantConnector.connect();
+        }
+    }
+
+    private void disconnect() {
+        if (merchantConnector != null) {
+            merchantConnector.disconnect();
+            merchantConnector = null;
+        }
+    }
+
+    private void getMerchant() {
+        new AsyncTask<Void, Void, Merchant>() {
+            @Override
+            protected Merchant doInBackground(Void... params) {
+                Merchant merchant = null;
+                try {
+                    merchant = merchantConnector.getMerchant();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                } catch (ClientException e) {
+                    e.printStackTrace();
+                } catch (ServiceException e) {
+                    e.printStackTrace();
+                } catch (BindingException e) {
+                    e.printStackTrace();
+                }
+                return merchant;
+            }
+
+            @Override
+            protected void onPostExecute(Merchant merchant) {
+                super.onPostExecute(merchant);
+
+                if (!isFinishing()) {
+                    merchantID = merchant.getId();
+                    merchantAddress = merchant.getAddress().toString(); // see what this does
+                    merchantPhone = merchant.getPhoneNumber();
+                }
+            }
+        }.execute();
+    }
 
     private void sendInfoToURL(String url) {
         JSONObject jsonObject = new JSONObject();
         try {
-            // TODO: fill in actual data once merchant sign in is taken care of
             jsonObject.accumulate("job_title", jobNameEditText.getText());
             jsonObject.accumulate("job_desc", descriptionEditText.getText());
-            jsonObject.accumulate("merch_id", "<merch_id_here>");
-            jsonObject.accumulate("from_loc", "<from_loc_here>");
+            jsonObject.accumulate("merch_id", merchantID);
+            jsonObject.accumulate("from_loc", merchantAddress); // not sure if this is right
             jsonObject.accumulate("to_loc", "<to_loc_here>");
-            jsonObject.accumulate("bus_phone", "<bus_phone_here>");
+            jsonObject.accumulate("bus_phone", merchantPhone);
         } catch (JSONException ex) {
             // this will never happen
         }
